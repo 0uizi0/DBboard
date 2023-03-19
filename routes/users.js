6d0cd62c3fcec32b7721e9364b05d57f17f17b93
0uizi0 <0uizi0@naver.com>
2023-03-19 17:23:10 +0900

    기능추가: 유저 뷰 파일 생성 및 연결

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('user Router');
+  res.render('user', {user: 'admin'});
 });
 
 module.exports = router;
