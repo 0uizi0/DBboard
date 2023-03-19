fd3f54979e8c2cae018252f20c71521ea25fc904
0uizi0 <0uizi0@naver.com>
2023-03-19 17:12:25 +0900

    기능추가: 유저 라우터 생성 및 서버 연결

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const PORT = 4000;
 app.use(cors());
 
 const mainRouter = require('./routes');
+const userRouter = require('./routes/users');
 
 app.use('/', mainRouter);
+app.use('/users', userRouter);
 
 app.listen(PORT, () => {
   console.log(`${PORT}번 포트에서 서버 실행 중`);
