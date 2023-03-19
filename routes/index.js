2bf093906de98fbd4413aa57ad7a4e5ede0dd3c9
0uizi0 <0uizi0@naver.com>
2023-03-19 17:20:47 +0900

    기능추가: 메인 뷰 파일 생성 및 연결

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('main Router');
+  res.render('index', {msg: '데이터 전달'});
 });
 
 module.exports = router;
