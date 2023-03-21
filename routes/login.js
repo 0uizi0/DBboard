b99b2b0cd1eefad2e3a1a9384651f253bc2c8374
0uizi0 <0uizi0@naver.com>
2023-03-22 00:12:03 +0900

    기능추가: 로그아웃 라우터 작성

--- routes/login.js
+++ routes/login.js
@@ -24,4 +24,11 @@ router.post('/', (req, res) => {
   });
 })
 
+router.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) throw err;
+    res.redirect('/');
+  });
+});
+
 module.exports = router;
