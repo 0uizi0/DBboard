9ccd78a9ee5ddacc475e19695f4c8c279ca512d2
0uizi0 <0uizi0@naver.com>
2023-03-21 23:30:06 +0900

    기능추가: 회원가입용 라우터 구현

--- app.js
+++ app.js
@@ -31,12 +31,14 @@ const userRouter = require('./routes/users');
 const boardRouter = require('./routes/board');
 const dbUserRouter = require('./routes/dbUser');
 const cookieRouter = require('./routes/cookie');
+const registerRouter = require('./routes/register');
 
 app.use('/', mainRouter);
 app.use('/users', userRouter);
 app.use('/board', boardRouter);
 app.use('/dbUser', dbUserRouter);
 app.use('/cookie', cookieRouter);
+app.use('/register', registerRouter);
 
 app.listen(PORT, () => {
   console.log(`${PORT}번 포트에서 서버 실행 중`);
