87ee4c4c3ce91fe088bc0cbc5f0b29b2dee74ad3
0uizi0 <0uizi0@naver.com>
2023-03-21 23:44:06 +0900

    기능추가: 회원 가입 라우터 작성

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const userDB = require('../controllers/userController');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('register');
 });
+
+router.post('/', (req, res) => {
+  userDB.userCheck(req.body.id, (data) => {
+    if (data.length === 0) {
+      userDB.registerUser(req.body, (result) => {
+        if (result.affectedRows >= 1) {
+          res.status(200).send('회원 가입에 성공하였습니다.<br/><a href="/login">로그인 페이지로 이동</a>');
+        } else {
+          res.status(404).send('회원 가입에 실패하였습니다.<br/><a href="/register">회원가입 페이지로 이동</a>');
+        }
+      });
+    } else {
+      res.status(400).send('동일한 ID를 가진 회원이 존재합니다. <br/><a href="/register">회원가입으로 이동</a>');
+    }
+  })
+})
 
 module.exports = router;
