ce6c7e55a6a762c36bf5fafd7b6466308c8bd38a
0uizi0 <0uizi0@naver.com>
2023-03-23 00:57:40 +0900

    기능변경: 회원가입 컨트롤러 mongoDB 연결

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,46 @@
-const connection = require('./dbConnect');
+const MongoClient = require('./mongoConnect');
 
 const userDB = {
-  getUsers: (cb) => {
-    connection.query('SELECT * FROM board_db.user;', (err,data) => {
-      if (err) throw err;
-      console.log(data);
-      cb(data);
-    });
+  userCheck: async (userId) => {
+    const client  = await MongoClient.connect();
+    const user = client.db('mongo').collection('user');
+
+    const findUser  = await user.findOne({id: userId});
+    if (!findUser) return false;
+    return findUser;
   },
-  userCheck: (userId, cb) => {
-    connection.query(`SELECT * FROM board_db.user WHERE USERID = '${userId}';`, (err, data) => {
-      if (err) throw err;
-      console.log(data);
-      cb(data);
-    })
-  },
-  registerUser: (newUser, cb) => {
-    connection.query(`INSERT INTO board_db.user (USERID, PASSWORD) values ('${newUser.id}', '${newUser.password}');`, (err, data) => {
-      if (err) throw err;
-      console.log(data);
-      cb(data);
-    })
+  registerUser: async (newUser) => {
+    const client  = await MongoClient.connect();
+    const user = client.db('mongo').collection('user');
+
+    const insertResult = await user.insertOne(newUser);
+    if (!insertResult.acknowledged) throw new Error('회원 등록 실패');
+    return true;
   }
-};
+}
+
+// const userDB = {
+//   getUsers: (cb) => {
+//     connection.query('SELECT * FROM board_db.user;', (err,data) => {
+//       if (err) throw err;
+//       console.log(data);
+//       cb(data);
+//     });
+//   },
+//   userCheck: (userId, cb) => {
+//     connection.query(`SELECT * FROM board_db.user WHERE USERID = '${userId}';`, (err, data) => {
+//       if (err) throw err;
+//       console.log(data);
+//       cb(data);
+//     })
+//   },
+//   registerUser: (newUser, cb) => {
+//     connection.query(`INSERT INTO board_db.user (USERID, PASSWORD) values ('${newUser.id}', '${newUser.password}');`, (err, data) => {
+//       if (err) throw err;
+//       console.log(data);
+//       cb(data);
+//     })
+//   }
+// };
 
 module.exports = userDB;
