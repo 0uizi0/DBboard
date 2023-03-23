6cecd4c261b85c4fcbc13548916515d3f3eba5a3
0uizi0 <0uizi0@naver.com>
2023-03-24 00:54:36 +0900

    형식: 주석 제거

--- controllers/boardController.js
+++ controllers/boardController.js
@@ -92,40 +92,6 @@ const deletePost = async (req, res) => {
   }
 }
 
-// const boardDB = {
-  // getAllArticles: (cb) => {
-  //   connection.query('SELECT * FROM board_db.board;', (err, data) => {
-  //     if (err) throw err;
-  //     console.log(data);
-  //     cb(data);
-  //   });
-  // },
-  // writePost: (newPost, cb) => {
-  //   connection.query(`INSERT INTO board_db.board (USERID, TITLE, CONTENT) VALUES ('${newPost.id}','${newPost.title}', '${newPost.content}')`, (err, data) => {
-  //     if (err) throw err;
-  //     cb(data);
-  //   })
-  // },
-  // getPost: (id, cb) => {
-  //   connection.query(`SELECT * FROM board_db.board WHERE ID_PK= ${id}`, (err, data) => {
-  //     if (err) throw err;
-  //     cb(data);
-  //   })
-  // },
-  // modifyPost: (id, modifyPost, cb) => {
-  //   connection.query(`UPDATE board_db.board SET TITLE = '${modifyPost.title}', CONTENT = '${modifyPost.content}' WHERE ID_PK = ${id};`, (err, data) => {
-  //     if (err) throw err;
-  //     cb(data);
-  //   });
-  // },
-  // deletePost: (id, cb) => {
-  //   connection.query(`DELETE FROM board_db.board WHERE ID_PK  = ${id};`, (err, data) => {
-  //     if (err) throw err;
-  //     cb(data);
-  //   })
-  // }
-// };
-
 module.exports = {
   getAllPosts,
   writePost,
--- controllers/userController.js
+++ controllers/userController.js
@@ -46,49 +46,6 @@ const loginUser = async (req, res) => {
   }
 };
 
-// const userDB = {
-//   userCheck: async (userId) => {
-//     const client  = await mongoClient.connect();
-//     const user = client.db('mongo').collection('user');
-
-//     const findUser  = await user.findOne({id: userId});
-//     if (!findUser) return false;
-//     return findUser;
-//   },
-  // registerUser: async (newUser) => {
-  //   const client  = await mongoClient.connect();
-  //   const user = client.db('mongo').collection('user');
-
-  //   const insertResult = await user.insertOne(newUser);
-  //   if (!insertResult.acknowledged) throw new Error('회원 등록 실패');
-  //   return true;
-  // }
-// }
-
-// const userDB = {
-//   getUsers: (cb) => {
-//     connection.query('SELECT * FROM board_db.user;', (err,data) => {
-//       if (err) throw err;
-//       console.log(data);
-//       cb(data);
-//     });
-//   },
-//   userCheck: (userId, cb) => {
-//     connection.query(`SELECT * FROM board_db.user WHERE USERID = '${userId}';`, (err, data) => {
-//       if (err) throw err;
-//       console.log(data);
-//       cb(data);
-//     })
-//   },
-//   registerUser: (newUser, cb) => {
-//     connection.query(`INSERT INTO board_db.user (USERID, PASSWORD) values ('${newUser.id}', '${newUser.password}');`, (err, data) => {
-//       if (err) throw err;
-//       console.log(data);
-//       cb(data);
-//     })
-//   }
-// };
-
 module.exports = {
   registerUser,
   loginUser,
